ee7b75029b97c1ec7d830e4ce32f9d14ec6fc8a6
Simza-ST <simzast123@gmail.com>
2025-05-13 14:01:38 +0200

    initial commit

---
+++ src/main/java/com/investhoodit/RevisionHub/RevisionHubApplication.java
@@ -0,0 +1,13 @@
+package com.investhoodit.RevisionHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevisionHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevisionHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/investhoodit/RevisionHub/RevisionHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.investhoodit.RevisionHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevisionHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
