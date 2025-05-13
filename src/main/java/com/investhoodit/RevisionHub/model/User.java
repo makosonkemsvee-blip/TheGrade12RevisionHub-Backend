3493ea8c914379d3f58f749872b9fe735db87854
Simza-ST <simzast123@gmail.com>
2025-05-13 14:46:37 +0200

    12/05/2025 update

---
+++ src/main/java/com/investhoodit/RevisionHub/model/User.java
@@ -0,0 +1,124 @@
+package com.investhoodit.RevisionHub.model;
+
+import java.util.List;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "user_tbl")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String idNumber;
+    private String phoneNumber;
+    private String email;
+    private String password;
+    @Lob
+    private byte[] profilePicture;
+    private String role;
+    @Column(name = "first_login", nullable = false)
+    private boolean firstLogin = true;
+
+    public User() {
+
+    }
+
+    public User(String firstName, String lastName, String idNumber,String phoneNumber,
+                String email, String password, String role) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.idNumber = idNumber;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+        this.firstLogin=true;;// Initialize firstLogin to true for new users
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getIdNumber() {
+        return idNumber;
+    }
+
+    public void setIdNumber(String idNumber) {
+        this.idNumber = idNumber;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public byte[] getProfilePicture() {
+        return profilePicture;
+    }
+
+    public void setProfilePicture(byte[] profilePicture) {
+        this.profilePicture = profilePicture;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public boolean isFirstLogin() {
+        return firstLogin;
+    }
+
+    public void setFirstLogin(boolean firstLogin) {
+        this.firstLogin = firstLogin;
+    }
+
+}
