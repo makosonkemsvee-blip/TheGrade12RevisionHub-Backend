6702893744a90af2810c390fe2298d84682c7e52
Simza-ST <simzast123@gmail.com>
2025-05-18 14:06:23 +0200

    Profile Management updated

--- src/main/java/com/investhoodit/RevisionHub/controller/UserSignupController.java
+++ src/main/java/com/investhoodit/RevisionHub/controller/UserSignupController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/signup")
+@RequestMapping("/api/signup")
 public class UserSignupController {
 
     private final UserSignupService userSignupService;
